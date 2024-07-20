134ab3d6940e478c9a1edcb44b90a4ccdb2ed81f
Olga Blud <olga.blud@gmail.com>
2024-07-20 09:13:07 +0300

    add profile styles

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,24 +1,28 @@
+import css from "./Profile.module.css";
 function Profile({ name, tag, location, image, stats }) {
   return (
-    <div>
-      <div>
-        <img src={image} alt="User avatar" />
-        <p>{name}</p>
-        <p>@{tag}</p>
-        <p>{location}</p>
+    <div className={css.profileWrap}>
+      <div className={css.profileMainInfoWrap}>
+        <img className={css.profileIcon} src={image} alt="User avatar" />
+        <p className={css.userName}>{name}</p>
+        <p className={css.userDescription}>@{tag}</p>
+        <p className={css.userDescription}>{location}</p>
       </div>
-      <ul>
-        <li>
-          <span>Followers</span>
-          <span>{stats.followers}</span>
+      <ul className={css.profileList}>
+        <li className={css.profileListItem}>
+          <span className={css.profileIndicators}>Followers</span>
+          <br />
+          <span className={css.profileStatistic}>{stats.followers}</span>
         </li>
-        <li>
-          <span>Views</span>
-          <span>{stats.views}</span>
+        <li className={css.profileListItem}>
+          <span className={css.profileIndicators}>Views</span>
+          <br />
+          <span className={css.profileStatistic}>{stats.views}</span>
         </li>
-        <li>
-          <span>Likes</span>
-          <span>{stats.likes}</span>
+        <li className={css.profileListItem}>
+          <span className={css.profileIndicators}>Likes</span>
+          <br />
+          <span className={css.profileStatistic}>{stats.likes}</span>
         </li>
       </ul>
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+// import "node_modules/modern-normalize/modern-normalize.css";
 import "./index.css";
 import App from "./components/App.jsx";
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
