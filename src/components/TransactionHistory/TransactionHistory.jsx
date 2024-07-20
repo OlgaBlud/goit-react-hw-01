eb6b6534afa84484368bdab4f46d6fbbc2753d4c
Olga Blud <olga.blud@gmail.com>
2024-07-20 18:31:55 +0300

    add transaction styles

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,24 +1,25 @@
+import css from "./TransactionHistory.module.css";
 function TransactionHistory({ transactions }) {
   return (
     <>
       <h2 className="taskTitle">Transaction History</h2>
 
-      <table>
-        <thead>
-          <tr>
-            <th>Type</th>
-            <th>Amount</th>
-            <th>Currency</th>
+      <table className={css.table}>
+        <thead className={css.tableHead}>
+          <tr className={css.tableHeadRow}>
+            <th className={css.tableHeadTitle}>Type</th>
+            <th className={css.tableHeadTitle}>Amount</th>
+            <th className={css.tableHeadTitle}>Currency</th>
           </tr>
         </thead>
 
         <tbody>
           {transactions.map(({ id, type, amount, currency }) => {
             return (
-              <tr key={id}>
-                <td>{type}</td>
-                <td>{amount}</td>
-                <td>{currency}</td>
+              <tr className={css.tableRow} key={id}>
+                <td className={(css.tableColumn, css.transName)}>{type}</td>
+                <td className={css.tableColumn}>{amount}</td>
+                <td className={css.tableColumn}>{currency}</td>
               </tr>
             );
           })}
